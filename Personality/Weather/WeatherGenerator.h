1c988cc3a23b982d142c4565a759d8f6016dca92
ShumWengSang <ballwallcallall@gmail.com>
2015-02-18 12:10:48 +0800

    Weather generater.
    Weather generated. Trying to add turbulence and make it usable.

--- Personality/Weather/WeatherGenerator.h
+++ Personality/Weather/WeatherGenerator.h
@@ -1,11 +1,20 @@
 #pragma once
 #include <vector>
+#include <iostream>
+#define noiseWidth 42
 class WeatherGenerator
 {
 public:
 	WeatherGenerator();
 	~WeatherGenerator();
-	std::vector<int> HourlyHaze
-	
+	std::vector<float> HourlyHaze;
+	std::vector<float> HazeGraph;
+	void PrintOutHourly();
+	void PrintOutGraph();
+
+	//double noise[noiseWidth];
+	double turbulence(double x, double size);
+
+	double interpolation(double x);
 };
 
